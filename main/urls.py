76f0c27feacc4a034cb8d95c8bdb770b0f277650
LakshanChinthaka <chinthaka21022@gmail.com>
2023-09-20 22:52:16 +0530

    Login, Cart, cheackout, cartlist, total amount

--- main/urls.py
+++ main/urls.py
@@ -6,13 +6,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('search',views.search,name='search'),
     path('category_list',views.category_list,name='category_list'),
     path('brand_list',views.brand_list,name='brand_list'),
     path('product_list',views.product_list,name='product_list'),
     path('category_product_list/<int:cat_id>',views.category_product_list,name='category_product_list'),
     path('brand_product_list/<int:brand_id>',views.brand_product_list,name='brand_product_list'),
-    path('product/<str:slug>/<int:id>/', views.product_detail, name='product_detail')
-
+    path('product/<str:slug>/<int:id>/',views.product_detail, name='product_detail'),
+    path('filter-data',views.filter_data,name='filter_data'),
+    path('load-more-data',views.load_more_data,name='load_more_data'),
     
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse,HttpResponse
+from django.template.loader import render_to_string
+from django.db.models import Max,Min,Count
 from .models import Category
 from .models import Brand
 from .models import Product
@@ -24,13 +27,16 @@ def brand_list(request):
 
 #Product list   viwe all products and filter
 def product_list(request):
-    data=Product.objects.all().order_by('-id')
-    # return render(request,'product_list.html',{'data':data})
+    total_data=Product.objects.count()
+    data=Product.objects.all().order_by('-id')[:3]  #[:10] is 10 products is display after load more display another products
     return render(request,'product_list.html',
         {
             'data':data,
+            'total_data':total_data,
+            # 'min_price':min_price,
+            # 'max_price':max_price,
         }         
-        )
+)
 
 #Product list according to Category
 def category_product_list(request,cat_id):
@@ -46,6 +52,55 @@ def brand_product_list(request,brand_id):
 
 #Product details
 def product_detail(request,slug,id):
-    slug=Product.objects.get(slug=slug)
-    data=Product.objects.get(id=id)
-    return render(request,'product_detail.html',{'data':data})
+    product=Product.objects.get(id=id)
+    related_products=Product.objects.filter(category=product.category).exclude(id=id)[:4]
+    return render(request,'product_detail.html',{'data':product,'related': related_products})
+    # return render(request, 'product_detail.html', {'data': product})
+
+#Search
+def search(request):
+    q=request.GET['q']
+    data=Product.objects.filter(title__icontains=q).order_by('-id')
+    return render(request,'search.html',{'data':data})
+
+#filter data
+def filter_data(request):
+    colors = request.GET.getlist('color[]')
+    categories = request.GET.getlist('category[]')
+    brands = request.GET.getlist('brand[]')
+    sizes = request.GET.getlist('size[]')
+    minPrice = request.GET['minPrice']
+    maxPrice = request.GET['maxPrice']
+
+    # Start with all products
+    allProducts = Product.objects.all().order_by('-id').distinct()
+
+    # Apply price range filter
+    allProducts = allProducts.filter(productattribute__price__gte=minPrice)
+    allProducts = allProducts.filter(productattribute__price__lte=maxPrice)
+
+    if colors:
+        allProducts = allProducts.filter(productattribute__color__id__in=colors).distinct()
+
+    if categories:
+        allProducts = allProducts.filter(category__id__in=categories).distinct()
+
+    if brands:
+        allProducts = allProducts.filter(brand__id__in=brands).distinct()
+
+    if sizes:
+        allProducts = allProducts.filter(productattribute__sizes__id__in=sizes).distinct()
+
+    # Render the filtered products using a template
+    data = render_to_string('ajax/product_list.html', {'data': allProducts})
+
+    return JsonResponse({'data': data})
+
+#load more
+def load_more_data (request):
+    offset=int(request.GET['offset'])
+    limit=int(request.GET['limit'])
+    data=Product.objects.all().order_by('-id')[offset:offset+limit]
+    data = render_to_string('ajax/product_list.html', {'data': data})
+    return JsonResponse({'data': data})
+
